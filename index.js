8a7e43d29a5a532cccbeab28c22b95b727c76763
Tofail Ahmed Sayem <atofail50@gmail.com>
2023-06-11 19:47:48 +0600

    admin api added

--- index.js
+++ index.js
@@ -10,25 +10,25 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// const verifyJWT = (req, res, next) => {
-//       const authorization = req.headers.authorization;
-//       if (!authorization) {
-//             return res.status(401).send({ error: true, message: "unauthorized access" })
-//       }
-//       const token = authorization.split(' ')[1];
+const verifyJWT = (req, res, next) => {
+      const authorization = req.headers.authorization;
+      if (!authorization) {
+            return res.status(401).send({ error: true, message: "unauthorized access" })
+      }
+      const token = authorization.split(' ')[1];
 
-//       jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-//             if (err) {
-//                   return res.status(401).send({ error: true, message: "unauthorized access" })
-//             }
-//             req.decoded = decoded;
-//             next();
-//       })
-// }
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+            if (err) {
+                  return res.status(401).send({ error: true, message: "unauthorized access" })
+            }
+            req.decoded = decoded;
+            next();
+      })
+}
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zhsy6ko.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -108,6 +108,35 @@ async function run() {
             app.get('/users', async (req, res) => {
                   const result = await userCollection.find().toArray();
                   res.send(result)
+            })
+
+            app.patch('/users/admin/:id', async (req, res) => {
+                  const id = req.params.id;
+                  console.log(id);
+                  const filter = { _id: new ObjectId(id) };
+                  const updateDoc = {
+                        $set: {
+                              role: 'admin'
+                        },
+                  };
+
+                  const result = await userCollection.updateOne(filter, updateDoc);
+                  res.send(result);
+
+            })
+            app.patch('/users/instructor/:id', async (req, res) => {
+                  const id = req.params.id;
+                  console.log(id);
+                  const filter = { _id: new ObjectId(id) };
+                  const updateDoc = {
+                        $set: {
+                              role: 'instructor'
+                        },
+                  };
+
+                  const result = await userCollection.updateOne(filter, updateDoc);
+                  res.send(result);
+
             })
 
             // Send a ping to confirm a successful connection
